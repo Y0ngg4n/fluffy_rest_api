908e16084db3b153794be90cafb31e698ef97803
Yonggan <yonggan@oblivioncoding.pro>
2021-05-22 22:23:21 +0200

    Added basic connection managment to the database.

---
+++ src/db.rs
@@ -0,0 +1,46 @@
+use scylla::{IntoTypedRows, Session, SessionBuilder, QueryResult};
+use std::error::Error;
+use scylla::transport::errors::{NewSessionError, QueryError};
+
+pub async fn connect() -> Result<Session, Box<dyn Error>> {
+    println!("Connecting to scylla db ...");
+    let uri = std::env::var("SCYLLA_URI")
+        .unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+
+    let session = SessionBuilder::new()
+        .known_node(uri)
+        .build()
+        .await?;
+    // match session{
+    //     Ok(_) => {println!("Connected to database!")}
+    //     Err(e) => {panic!("Could not connect to database!")}
+    // }
+    Ok(session)
+}
+
+pub async fn create_keyspace_and_tables(session: Session) -> Result<(), Box<dyn Error>>{
+    println!("Creating Keyspace and tables...");
+    session
+        .query(
+            "CREATE KEYSPACE IF NOT EXISTS fluffy_board WITH REPLICATION = \
+            {'class' : 'SimpleStrategy', 'replication_factor' : 1}",
+            &[],
+        )
+        .await?;
+
+    session
+        .query(
+            "CREATE TABLE IF NOT EXISTS fluffy_board.whiteboard (\
+            id UUID primary key,\
+            name text,\
+            owner text,\
+            group text, \
+            password text,\
+            created timestamp, \
+            retention timestamp \
+            )",
+            &[],
+        )
+        .await?;
+    Ok(())
+}
---
+++ src/whiteboard.rs
@@ -0,0 +1,41 @@
+use actix_web::web::{Data, Json, Path};
+use actix_web::{web, HttpResponse, post};
+use crate::response::Response;
+use serde::{Deserialize, Serialize};
+
+use crate::constants::{APPLICATION_JSON};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Whiteboard {
+    pub id: String,
+}
+
+impl Whiteboard {
+    pub fn new(message: String) -> Self {
+        Self {
+            id: message,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct WhiteboardRequest {
+    pub message: Option<String>,
+}
+
+impl WhiteboardRequest {
+    pub fn to_whiteboard(&self) -> Option<Whiteboard> {
+        match &self.message {
+            Some(message) => Some(Whiteboard::new(message.to_string())),
+            None => None,
+        }
+    }
+}
+
+/// create a whiteboard `/whiteboard/create`
+#[post("/whiteboard/create")]
+pub async fn create(whiteboard_req: Json<WhiteboardRequest>) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type(APPLICATION_JSON)
+        .json(Whiteboard{id: String::from("WhiteboardTest") })
+}
